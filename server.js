dc89e48fd75067b161fac8f293aa9627e3f9b345
Josh <you@example.com>
2023-04-01 22:55:10 -0400

    server edits for deployment

--- server.js
+++ server.js
@@ -106,6 +106,6 @@ app.get('/login', (req, res) => {
   res.sendFile('login.html', { root: 'public' });
 });
 
-app.listen(3000, () => {
-  console.log('Server started on port 3000');
+const server = app.listen(process.env.PORT || 3000, () => {
+  console.log('Server started on port', server.address().port);
 });
